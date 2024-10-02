8ba8cabfc736f39e3dea1ff91a573bea0189a3a0
Matthew Somerville <matthew@mysociety.org>
2024-10-02 18:27:33 +0100

    Remove reliance on fixed Price IDs.
    Switch to using metadata on the price to get the number of calls for a
    price, rather than assuming it can be worked out from the price (plan)
    ID.

--- mapit_mysociety_org/settings.py
+++ mapit_mysociety_org/settings.py
@@ -181,8 +181,15 @@ RETRY_INTERVAL = 0
 BULK_LOOKUP_PRICE = 50
 
 # API subscriptions
-PRICING = [
-    {'id': 'mapit-10k-v', 'price': 20, 'calls': '10,000'},
-    {'id': 'mapit-100k-v', 'price': 100, 'calls': '100,000'},
-    {'id': 'mapit-0k-v', 'price': 300, 'calls': '0'},
-]
+if 'test' in sys.argv:
+    PRICING = [
+        {'id': 'price_123', 'price': 20, 'calls': '10,000'},
+        {'id': 'price_456', 'price': 100, 'calls': '100,000'},
+        {'id': 'price_789', 'price': 300, 'calls': '0'},
+    ]
+else:
+    PRICING = [
+        {'id': config.get('PRICING_TIER_1_ID'), 'price': 20, 'calls': '10,000'},
+        {'id': config.get('PRICING_TIER_2_ID'), 'price': 100, 'calls': '100,000'},
+        {'id': config.get('PRICING_TIER_3_ID'), 'price': 300, 'calls': '0'},
+    ]
